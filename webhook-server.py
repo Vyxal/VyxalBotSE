3a37633c1f73b1b89bb176d0f8c831e034dc57e3
Alexander Liao <alex.yj.liao@gmail.com>
2021-07-13 22:36:02 -0400

    make the code actually work

--- webhook-server.py
+++ webhook-server.py
@@ -198,17 +198,14 @@ def receive_message():
       return ""
     else:
       return f"{reply} you are not a privileged user; ask someone if you beleive you should be (user id: {message['user_id']})"
-  if re.match(r"blame$", without_ping):
+  if re.match(r"^blame$", without_ping):
     return f"{reply} It was {random.choice(['wasif', 'Underslash', 'math', 'Aaron Miller', 'A username', 'user', 'Unrelated String', 'AviFS', 'Razetime', 'lyxal', '2x-1', 'hyper-neutrino'])}'s fault!"
- 
-  if re.match(r"(hello|howdy|mornin['g]|evenin['g])", without_ping):
+  if re.match(r"^(hello|howdy|mornin['g]|evenin['g])$", without_ping):
     return f"{reply} hello to you too!"
-
-  if re.match(r"((good)?bye|see ya\!?|'night|goodnight)", without_ping):
+  if re.match(r"^((good)?bye|see ya\!?|'night|goodnight)$", without_ping):
     return f"{reply} o/"
-
-  if re.match(r"flowey quote", without_ping):
-    return f"{reply} {random.choice(["Howdy, I'm FLOWEY. FLOWEY the FLOWER", "In this world, it's KILL or BE killed.", "Hehehe, you really ARE an idiot.", "Clever...verrrry clever. You think you're really smart, don't you.", "Is this a joke? Are you braindead? RUN INTO THE BULLETS!!!", "I've read every book. I've burned every book. I've won every game. I've lost every game. I've appeased everyone. I've killed everyone. Sets of numbers... Lines of dialog... I've seen them all.", "You...! I'll keep you here no matter what! _Even if it means killing you 1,000,000 times!", "Down here, LOVE is shared through little white... 'friendliness pellets'", "Hehehe... did you REALLY think you could defeat ME?"])}" 
+  if re.match(r"^flowey quote$", without_ping):
+    return f"{reply} %s" % random.choice(["Howdy, I'm FLOWEY. FLOWEY the FLOWER", "In this world, it's KILL or BE killed.", "Hehehe, you really ARE an idiot.", "Clever...verrrry clever. You think you're really smart, don't you.", "Is this a joke? Are you braindead? RUN INTO THE BULLETS!!!", "I've read every book. I've burned every book. I've won every game. I've lost every game. I've appeased everyone. I've killed everyone. Sets of numbers... Lines of dialog... I've seen them all.", "You...! I'll keep you here no matter what! _Even if it means killing you 1,000,000 times!", "Down here, LOVE is shared through little white... 'friendliness pellets'", "Hehehe... did you REALLY think you could defeat ME?"]) 
   match = re.match(r"^(pro|de)mote (\d+)", without_ping)
   if match:
     if message["user_id"] not in STORAGE["admin"]:
