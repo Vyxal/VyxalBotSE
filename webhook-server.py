59d85785d1cb4a7f10b638932c1571b54461f8da
Lyxal <36217120+Lyxal@users.noreply.github.com>
2021-07-17 15:56:40 +1000

    geno quotes

--- webhook-server.py
+++ webhook-server.py
@@ -239,7 +239,9 @@ def receive_message():
                                             "What? Do you really think you can stop ME? Hee hee hee...you really ARE an idiot.", "Boy! What a shame! Nobody else...is going to get to see you DIE!!!",
                                             "What?! How'd you...?! Well I'll just. Wh...where are my powers?! The souls...? What are they doing? NO!! NO!!!!! YOU CAN'T DO THAT!!! YOU'RE SUPPOSED TO OBEY ME!! STOP!!! STOP IT!!!!! STOOOOPPPP!!!!!",
                                             "...What are you doing? Do you really think I've learned anything from this? No. Sparing me won't change anything.",
-                                            "And you know what the best part is? This is all YOUR fault!"])
+                                            "And you know what the best part is? This is all YOUR fault!", "Let's destroy everything in this wretched world. Everyone, everything in these worthless memories... Let's turn 'em all to dust.",
+                                           "_that's a wonderful idea!_", "At first, I used my powers for good. I became \"friends\" with everyone. I solved all their problems flawlessly. Their companionship was amusing... For a while.",
+                                           "It doesn't matter now. I'm so tired of this, Chara. I'm tired of all these people. I'm tired of all these places. I'm tired of being a flower."])
     if re.match(r"^repo(sitor(y|ies))? list$", without_ping):
       r = requests.get(f"https://api.github.com/orgs/Vyxal/repos", headers = {
         "Authorization": "token " + STORAGE["token"],
