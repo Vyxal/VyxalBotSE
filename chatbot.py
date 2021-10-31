4789b7d755ac72c3f9da74961ead777cccedec21
hyper-neutrino <alex.yj.liao@gmail.com>
2021-10-31 00:28:08 -0400

    fourth time's the charm?

--- chatbot.py
+++ chatbot.py
@@ -150,7 +150,7 @@ class Room():
                                 a = self.ws.recv()
                         except:
                                 log('Unexpected error for room {}; rebooting'.format(self.id))
-                                requests.get("localhost:5888/kill")
+                                requests.get("http://localhost:5888/kill")
                                 self.running=False
                         if a is not None and a != "":# not an empty messages
                                 a=json.loads(a)
